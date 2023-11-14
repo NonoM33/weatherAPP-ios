0552cbacf5519cd8e32aac65363db188b4c89fcf
Renaud cosson <renaudlemagicien@gmail.com>
2023-11-14 16:23:00 +0100

    add constants Colors
    update constant Colors

---
+++ weatherAPP-ios/weatherAPP-ios/Constants/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  weatherAPP-ios
+//
+//  Created by renaud on 13/11/2023.
+//
+
+import Foundation
+import UIKit
+
+enum Colors {
+    static let white: UIColor = .white
+    static let black: UIColor = .black
+}
