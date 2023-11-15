06deed880c2c79bd3789b034b59966396faac0d7
NonoM33 <renaudlemagicien@gmail.com>
2023-11-15 14:36:58 +0100

    add WeatherCustomKit (framework)

---
+++ weatherAPP-ios/weatherAPP-ios/Framework/WeatherKITCustom.framework/Headers/WeatherKITCustom.h
@@ -0,0 +1,18 @@
+//
+//  WeatherKITCustom.h
+//  WeatherKITCustom
+//
+//  Created by renaud on 15/11/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WeatherKITCustom.
+FOUNDATION_EXPORT double WeatherKITCustomVersionNumber;
+
+//! Project version string for WeatherKITCustom.
+FOUNDATION_EXPORT const unsigned char WeatherKITCustomVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeatherKITCustom/PublicHeader.h>
+
+
