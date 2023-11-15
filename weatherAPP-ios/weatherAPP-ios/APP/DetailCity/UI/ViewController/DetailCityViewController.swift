fedc6a3cbd77619328bd38cea0853ae397e2545f
Renaud cosson <renaudlemagicien@gmail.com>
2023-11-15 15:12:08 +0100

    add DetailCityViewContract + ViewController

---
+++ weatherAPP-ios/weatherAPP-ios/APP/DetailCity/UI/ViewController/DetailCityViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DetailCityViewController.swift
+//  weatherAPP-ios
+//
+//  Created by renaud on 15/11/2023.
+//
+
+import Foundation
+import UIKit
+
+class DetailCityViewController: UIViewController {
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+
+    // MARK: - Private
+
+    private func setup() {
+    }
+}
+
+extension DetailCityViewController: DetailCityViewContract {
+
+    // MARK: - DetailCityViewContract
+
+    func display(_ viweModel: DetailCityViewModel) {
+    }
+}
