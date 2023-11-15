31197d3c2ee88cb39e92c4dac466e53a5515b13d
Renaud cosson <renaudlemagicien@gmail.com>
2023-11-15 15:11:17 +0100

    create DetailCityView Model + Mapper

---
+++ weatherAPP-ios/weatherAPP-ios/APP/DetailCity/Model/DetailCityViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  DetailCityViewModel.swift
+//  weatherAPP-ios
+//
+//  Created by renaud on 15/11/2023.
+//
+
+import Foundation
+
+struct DetailCityViewModel {
+}
---
+++ weatherAPP-ios/weatherAPP-ios/APP/DetailCity/Model/Mapper/DetailCityViewModelMapper.swift
@@ -0,0 +1,17 @@
+//
+//  DetailCityViewModelMapper.swift
+//  weatherAPP-ios
+//
+//  Created by renaud on 15/11/2023.
+//
+
+import Foundation
+
+class DetailCityViewModelMapper {
+
+    // MARK: - DetailCityViewModelMapper
+
+    func map() -> DetailCityViewModel {
+        return DetailCityViewModel()
+    }
+}
