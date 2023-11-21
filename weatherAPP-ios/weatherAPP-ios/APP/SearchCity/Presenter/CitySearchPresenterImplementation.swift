9ded74cdd1a71e854804091533013b5b867412a7
NonoM33 <renaudlemagicien@gmail.com>
2023-11-21 14:33:19 +0100

    implemente selectCityWithCoordinates in CitySearchPresenterImpl

--- weatherAPP-ios/weatherAPP-ios/APP/SearchCity/Presenter/CitySearchPresenterImplementation.swift
+++ weatherAPP-ios/weatherAPP-ios/APP/SearchCity/Presenter/CitySearchPresenterImplementation.swift
@@ -6,16 +6,23 @@
 //
 
 import Foundation
+import WeatherPackage
 
 class CitySearchPresenterImplementation: CitySearchPresenter {
 
     private weak var delegate: CitySearchPresenterDelegate?
     private var viewContract: CitySearchViewContract?
+    private var navigationManager: NavigationManager
+    private var weatherCityRepository: WeatherCityRepository
 
     init(delegate: CitySearchPresenterDelegate,
-         viewContract: CitySearchViewContract) {
+         viewContract: CitySearchViewContract,
+         navigationManager: NavigationManager,
+         weatherCityRepository: WeatherCityRepository) {
         self.delegate = delegate
         self.viewContract = viewContract
+        self.navigationManager = navigationManager
+        self.weatherCityRepository = weatherCityRepository
     }
 
     // MARK: - CitySearchPresenter
@@ -24,5 +31,20 @@ class CitySearchPresenterImplementation: CitySearchPresenter {
         viewContract?.display(CitySearchViewModelMapper().map())
     }
 
-    func selectCityWithCoordinates(lon: Double, lat: Double) {}
+    func selectCityWithCoordinates(lon: Double, lat: Double) {
+        weatherCityRepository.getByCoordonate(lon: lon, lat: lat) { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let weatherEntitie):
+                let coordinator = DetailCityCoordinator(
+                    navigationController:
+                        self.navigationManager.mainViewController,
+                    weatherCityEntitie: weatherEntitie
+                )
+                coordinator.start()
+            case .failure(let failure):
+                print(failure)
+            }
+        }
+    }
 }
