539abb6832fb9c18471539c48ad89d9530b9bb39
NonoM33 <renaudlemagicien@gmail.com>
2023-11-21 14:33:53 +0100

    add DetailCityViewModel + mapper

--- weatherAPP-ios/weatherAPP-ios/APP/DetailCity/Model/DetailCityViewModel.swift
+++ weatherAPP-ios/weatherAPP-ios/APP/DetailCity/Model/DetailCityViewModel.swift
@@ -6,6 +6,32 @@
 //
 
 import Foundation
+import UIKit
 
 struct DetailCityViewModel {
+    let cityName: String
+    let date: String
+    let temperature: String
+    let description: String
+    let rainMm: String
+    let windKmh: String
+    let humidityPercent: String
+    let UVIndex: String
+    let icon: UIImage?
+    let backgroundColor: UIColor
+    var imageCity: UIImage?
+
+    static let empty = Self(
+        cityName: "",
+        date: "",
+        temperature: "",
+        description: "",
+        rainMm: "",
+        windKmh: "",
+        humidityPercent: "",
+        UVIndex: "",
+        icon: nil,
+        backgroundColor: .white,
+        imageCity: nil
+    )
 }
--- weatherAPP-ios/weatherAPP-ios/APP/DetailCity/Model/Mapper/DetailCityViewModelMapper.swift
+++ weatherAPP-ios/weatherAPP-ios/APP/DetailCity/Model/Mapper/DetailCityViewModelMapper.swift
@@ -9,9 +9,27 @@ import Foundation
 
 class DetailCityViewModelMapper {
 
+    private let weatherCityEntitie: WeatherCityEntitie
+
+    init(weatherCityEntitie: WeatherCityEntitie) {
+        self.weatherCityEntitie = weatherCityEntitie
+    }
+
     // MARK: - DetailCityViewModelMapper
 
     func map() -> DetailCityViewModel {
-        return DetailCityViewModel()
+        return DetailCityViewModel(
+            cityName: weatherCityEntitie.cityName,
+            date: weatherCityEntitie.date,
+            temperature: weatherCityEntitie.temperature,
+            description: weatherCityEntitie.description,
+            rainMm: weatherCityEntitie.rainMm,
+            windKmh: weatherCityEntitie.windKmh,
+            humidityPercent: weatherCityEntitie.humidityPercent,
+            UVIndex: weatherCityEntitie.UVIndex,
+            icon: weatherCityEntitie.icon,
+            backgroundColor: weatherCityEntitie.backgroundColor,
+            imageCity: nil
+        )
     }
 }
