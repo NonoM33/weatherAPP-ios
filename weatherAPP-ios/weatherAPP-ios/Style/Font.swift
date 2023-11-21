25b1360638e8c9e0fd81a43589a5a1c35c5ffa03
NonoM33 <renaudlemagicien@gmail.com>
2023-11-21 17:31:54 +0100

    update title2 Font change size

--- weatherAPP-ios/weatherAPP-ios/Style/Font.swift
+++ weatherAPP-ios/weatherAPP-ios/Style/Font.swift
@@ -13,7 +13,7 @@ extension UIFont {
         static var body2 = UIFont.systemFont(ofSize: 16, weight: .bold)
         static var body3 = UIFont.systemFont(ofSize: 14, weight: .bold)
         static var title1 = UIFont.systemFont(ofSize: 16, weight: .bold)
-        static var title2 = UIFont.systemFont(ofSize: 32, weight: .bold)
+        static var title2 = UIFont.systemFont(ofSize: 36, weight: .bold)
         static var title3 = UIFont.systemFont(ofSize: 40, weight: .regular)
         static var title4 = UIFont.systemFont(ofSize: 64, weight: .semibold)
         static var description1 = UIFont.systemFont(ofSize: 12, weight: .regular)
