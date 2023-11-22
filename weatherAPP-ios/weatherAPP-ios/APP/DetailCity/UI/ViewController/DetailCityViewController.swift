703f5ce749adb4c19120cfcde051e87e755f9ecf
NonoM33 <renaudlemagicien@gmail.com>
2023-11-22 16:06:03 +0100

    update DetailCityViewController

--- weatherAPP-ios/weatherAPP-ios/APP/DetailCity/UI/ViewController/DetailCityViewController.swift
+++ weatherAPP-ios/weatherAPP-ios/APP/DetailCity/UI/ViewController/DetailCityViewController.swift
@@ -7,12 +7,20 @@
 
 import Foundation
 import UIKit
+import WeatherPackage
 
 class DetailCityViewController: UIViewController {
 
     var presenter: DetailCityViewPresenter?
+    private lazy var stackView = UIStackView(style: .default)
+    private lazy var divider = createDivider()
+    private lazy var detailLocationView = DetailLocationView()
     private lazy var detailTemperatureView = DetailTemperatureView()
     private lazy var detailStatsInfoView = DetailStatsInfoView()
+    private lazy var detailTimeLineTempView = DetailTimeLineTempView()
+    private lazy var detailFuturTempDaily = DetailFuturTempDailyView()
+    private lazy var temperatureLabel = UILabel()
+    private lazy var imageCity = UIImageView(style: .rounded)
 
     // MARK: - Lifecycle
 
@@ -22,11 +30,81 @@ class DetailCityViewController: UIViewController {
         presenter?.start()
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .clear
+        appearance.backButtonAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.white]
+        appearance.setBackIndicatorImage(
+            UIImage(
+                named: "back_arrow"
+            ),
+            transitionMaskImage: UIImage(
+                named: "back_arrow"
+            )
+        )
+
+        self.navigationController?.navigationBar.standardAppearance = appearance
+        self.navigationController?.navigationBar.compactAppearance = appearance
+        self.navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        self.navigationController?.navigationBar.tintColor = Colors.white
+
+        if #available(iOS 15, *) {
+            self.navigationController?.navigationBar.compactScrollEdgeAppearance = appearance
+        }
+    }
+
     // MARK: - Private
 
     private func setup() {
+        self.navigationItem.backBarButtonItem = UIBarButtonItem(
+            title: "Retour",
+            style: .plain,
+            target: nil,
+            action: nil
+        )
+        view.addSubview(stackView)
+        setupStackView()
+    }
+
+    private func setupStackView() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.spacing = WeatherSpacing.Separator.large.value
+        stackView.addArrangedSubview(detailLocationView)
         stackView.addArrangedSubview(detailTemperatureView)
         stackView.addArrangedSubview(detailStatsInfoView)
+        stackView.addArrangedSubview(detailTimeLineTempView)
+        stackView.addArrangedSubview(divider)
+        stackView.addArrangedSubview(detailFuturTempDaily)
+        stackView.addArrangedSubview(UIView())
+
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            stackView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+
+    private func createDivider() -> UIView {
+        let view = UIView()
+        let divider = UIView()
+        divider.translatesAutoresizingMaskIntoConstraints = false
+        divider.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        divider.backgroundColor = Colors.white
+        view.addSubview(divider)
+
+        NSLayoutConstraint.activate([
+            divider.topAnchor.constraint(equalTo: view.topAnchor),
+            divider.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            divider.leftAnchor.constraint(
+                equalTo: view.leftAnchor,
+                constant: WeatherSpacing.EdgeInsets.horizontal.value.left
+            ),
+            divider.widthAnchor.constraint(equalToConstant: super.view.bounds.width / 1.1)
+        ])
+        return view
     }
 }
 
@@ -34,6 +112,14 @@ extension DetailCityViewController: DetailCityViewContract {
 
     // MARK: - DetailCityViewContract
 
-    func display(_ viweModel: DetailCityViewModel) {
+    func display(with viewModel: DetailCityViewModel) {
+        let gradientLayer = viewModel.backgroundColor
+        gradientLayer.frame = view.bounds
+        view.layer.insertSublayer(gradientLayer, at: 0)
+        detailLocationView.configure(with: viewModel.detailLocationViewModel)
+        detailTemperatureView.configure(with: viewModel.detailTemperatureViewModel)
+        detailStatsInfoView.configure(with: viewModel.detailStatsInfoViewModel)
+        detailTimeLineTempView.configure(with: viewModel.detailTimeLineTempViewmodel)
+        detailFuturTempDaily.configure(with: viewModel.detailFuturTempDailyViewModel)
     }
 }
