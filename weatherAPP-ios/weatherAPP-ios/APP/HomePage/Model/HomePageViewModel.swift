b35bdca597042462c8e14325c888903bbd7dc78e
NonoM33 <renaudlemagicien@gmail.com>
2023-11-24 11:14:42 +0100

    HomePage ViewModel + Mapper

---
+++ weatherAPP-ios/weatherAPP-ios/APP/HomePage/Model/HomePageViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  HomePageViewModel.swift
+//  weatherAPP-ios
+//
+//  Created by renaud on 24/11/2023.
+//
+
+import Foundation
+
+struct HomePageViewModel {
+}
---
+++ weatherAPP-ios/weatherAPP-ios/APP/HomePage/Model/Mapper/HomePageViewModelMapper.swift
@@ -0,0 +1,15 @@
+//
+//  HomePageViewModelMapper.swift
+//  weatherAPP-ios
+//
+//  Created by renaud on 24/11/2023.
+//
+
+import Foundation
+
+final class HomePageViewModelMapper {
+
+    func map() -> HomePageViewModel {
+        return HomePageViewModel()
+    }
+}
