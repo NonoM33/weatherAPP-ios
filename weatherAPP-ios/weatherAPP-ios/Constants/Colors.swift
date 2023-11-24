196f9636f5bb2bd1988783bac0d2e1ccc5554615
NonoM33 <renaudlemagicien@gmail.com>
2023-11-24 11:15:56 +0100

    add  custom gray Color

--- weatherAPP-ios/weatherAPP-ios/Constants/Colors.swift
+++ weatherAPP-ios/weatherAPP-ios/Constants/Colors.swift
@@ -12,4 +12,6 @@ enum Colors {
     static let white: UIColor = .white
     static let black: UIColor = .black
     static let orange = UIColor(red: 0.99, green: 0.49, blue: 0.46, alpha: 1)
+    static let gray = UIColor(red: 25/255.0, green: 25/255.0, blue: 25/255.0, alpha: 1.0)
+
 }
