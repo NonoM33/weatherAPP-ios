0166df0f7765409bd04c44af2103bd4e659dd239
NonoM33 <renaudlemagicien@gmail.com>
2023-11-24 17:25:33 +0100

    update HomePageViewModel + mapper

--- weatherAPP-ios/weatherAPP-ios/APP/HomePage/Model/HomePageViewModel.swift
+++ weatherAPP-ios/weatherAPP-ios/APP/HomePage/Model/HomePageViewModel.swift
@@ -8,4 +8,5 @@
 import Foundation
 
 struct HomePageViewModel {
+    let cityFavoriteViewModel: CityFavoriteViewModel
 }
--- weatherAPP-ios/weatherAPP-ios/APP/HomePage/Model/Mapper/HomePageViewModelMapper.swift
+++ weatherAPP-ios/weatherAPP-ios/APP/HomePage/Model/Mapper/HomePageViewModelMapper.swift
@@ -10,6 +10,28 @@ import Foundation
 final class HomePageViewModelMapper {
 
     func map() -> HomePageViewModel {
-        return HomePageViewModel()
+        return HomePageViewModel(
+            cityFavoriteViewModel: mapCityFavoriteViewModel()
+        )
+    }
+
+    // MARK: - Private
+
+    private func mapCityFavoriteViewModel() -> CityFavoriteViewModel {
+        return CityFavoriteViewModel(cityFavoriteViewCellsModels: mapCityFavoriteViewCellsModels())
+    }
+
+    private func mapCityFavoriteViewCellsModels() -> [CityFavoriteViewCellModel] {
+        return [.init(
+            cityName: "Antony",
+            icon: ._01D,
+            temperature: "12°C",
+            lon: 2.2945486000000002,
+            lat: 48.752281000000004
+        )]
+    }
+
+    private func mapCityFavoriteViewCellModel() -> CityFavoriteViewCellModel {
+        return CityFavoriteViewCellModel(cityName: "", icon: ._01D, temperature: "12°C", lon: 30, lat: 30)
     }
 }
